594ce28e55523ec2598ee097e96230edd29ba245
Josue Hidalgo Bustamante <164217768+Kabudence@users.noreply.github.com>
2025-07-17 12:51:33 -0500

    feat: update duration in available slots test to 120 minutes for improved accuracy

--- test/test_find_avaliable_slots.py
+++ test/test_find_avaliable_slots.py
@@ -3,7 +3,7 @@ from schedules.domain.entities.schedule import Schedule
 from schedules.domain.services.schedule_service import find_available_slots
 
 schedule = Schedule(start_time="08:00", end_time="22:00")
-duration = 60
+duration = 120
 
 occupied_slots = [
     {'start_time': '08:00', 'end_time': '09:10'},
@@ -13,4 +13,3 @@ occupied_slots = [
 
 free_slots = find_available_slots(schedule, duration, occupied_slots)
 print(free_slots)
-# [{'start_time': '09:10', 'end_time': '09:35'}, {'start_time': '11:14', 'end_time': '16:20'}, {'start_time': '18:00', 'end_time': '22:00'}]
